5da9235967734a22b1b20c8eaa679ede2e4db651
Shahin Motia <chromepanthers@gmail.com>
2017-10-28 18:03:33 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,25 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
-let index = 0;
-
 function init() {
-  const keyStroke = parseInt(event.which);
+  let index = 0;
+  const body = getElementByTagName('body');
 
-  if (keyStroke === code[index]) {
-    console.log(keyStroke);
-    index++;
-    console.log(index)
-    if (index === code.length) {
-      alert("27 Extra Lives!");
+  body.addEventListener('keydown', function(event) {
+    const keyStroke = parseInt(event.which);
+
+    if (keyStroke === code[index]) {
+      console.log(keyStroke);
+      index++;
+      console.log(index)
+      if (index === code.length) {
+        alert("27 Extra Lives!");
+        index = 0;
+      }
+    }
+    else {
       index = 0;
     }
-  }
-  else {
-    index = 0;
-  }
+  })
 }
-
-const body = getElementByTagName('body');
-body.addEventListener('keydown', function(event) {
-  init();
-})
