8b6f86764d3869dabdf5cb4ddb9e17b2760b7717
Shahin Motia <chromepanthers@gmail.com>
2017-10-28 16:42:00 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 let index = 0;
 
 function init(e) {
-  var key = parseInt(e.detail || e.which);
+  var key = parseInt(e.which);
 
   if (key === code[0]) {
     console.log(key);
