441f8af2c96dc4bd12c81a6b4fad5862a58e3f44
Shahin Motia <chromepanthers@gmail.com>
2017-10-28 17:19:00 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 let index = 0;
-document.body.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function(event) {
   init()
 });
 
