cb6a7740c3f1f861f8d5b0fbe96a3aea6fed87ec
Shahin Motia <chromepanthers@gmail.com>
2017-10-28 17:02:38 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,8 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 let index = 0;
-const dabody = getElementByTagName('body')
-dabody.addEventListener('keydown', init());
+document.body.addEventListener('keydown', init());
 
 function init(e) {
   const key = parseInt(e.key || e.detail || e.which);
